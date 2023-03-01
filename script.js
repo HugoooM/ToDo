1ca85640ec49e92c252e8152bee462846fc2d477
Mr-JeJey <beatrix.e2100692@etud.univ-ubs.fr>
2023-03-01 12:25:37 +0100

    abc

--- script.js
+++ script.js
@@ -1,68 +1,41 @@
-// Initialisation de l'application Vue.js
+// Création de l'application Vue.js
 new Vue({
-	el: '#tasks-list',
-	data: {
-		tasks: []
-	},
-	methods: {
-		addTask: function() {
-			// Récupération des valeurs des champs du formulaire
-			var description = document.getElementById('description').value;
-			var startDate = document.getElementById('start-date').value;
-			var endDate = document.getElementById('end-date').value;
-			var status = document.getElementById('status').value;
-			var priority = document.getElementById('priority').value;
-			// Création de l'objet tâche
-			var task = {
-				description: description,
-				startDate: startDate,
-				endDate: endDate,
-				status: status,
-				priority: priority
-			};
-			// Ajout de la tâche à la liste des tâches
-			this.tasks.push(task);
-			// Sauvegarde des données dans le local storage
-			localStorage.setItem('tasks', JSON.stringify(this.tasks));
-			// Réinitialisation du formulaire
-			document.getElementById('task-form').reset();
-		},
-		removeTask: function(index) {
-			// Suppression de la tâche de la liste des tâches
-			this.tasks.splice(index, 1);
-			// Sauvegarde des données dans le local storage
-			localStorage.setItem('tasks', JSON.stringify(this.tasks));
-		},
-		getTasksByStatus: function(status) {
-			// Filtrage des tâches par état
-			return this.tasks.filter(function(task) {
-				return task.status === status;
-			});
-		},
-		getTasksByPriority: function(priority) {
-			// Filtrage des tâches par priorité
-			return this.tasks.filter(function(task) {
-				return task.priority === priority;
-			});
-		},
-		getTasksByStartDate: function() {
-			// Tri des tâches par date de début
-			return this.tasks.sort(function(task1, task2) {
-				return new Date(task1.startDate) - new Date(task2.startDate);
-			});
-		},
-		getTasksByEndDate: function() {
-			// Tri des tâches par date de fin
-			return this.tasks.sort(function(task1, task2) {
-				return new Date(task1.endDate) - new Date(task2.endDate);
-			});
-		}
-	},
-	mounted: function() {
-		// Récupération des données depuis le local storage
-		var tasksData = localStorage.getItem('tasks');
-		if (tasksData) {
-			this.tasks = JSON.parse(tasksData);
-		}
-	}
+    el: '#app',
+    data: {
+      tasks: [],
+      description: '',
+      startDate: '',
+      endDate: '',
+      status: 'À faire',
+      priority: 'Basse',
+    },
+    methods: {
+      addTask: function() {
+        const newTask = {
+          description: this.description,
+          startDate: this.startDate,
+          endDate: this.endDate,
+          status: this.status,
+          priority: this.priority,
+        };
+
+        this.tasks.push(newTask);
+        this.description = '';
+        this.startDate = '';
+        this.endDate = '';
+        this.status = 'À faire';
+        this.priority = 'Basse';
+
+        const message = document.getElementById('message');
+        message.style.display = 'block';
+        setTimeout(function() {
+          message.style.display = 'none';
+        }, 500);
+      },
+      removeTask: function(index) {
+        // Suppression de la tâche correspondante
+        this.tasks.splice(index, 1);
+      },
+    },
 });
+  
