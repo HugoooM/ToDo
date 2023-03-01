987e14d8e17984370e046c2e54211e57d5330a38
HugoooM <hugo.montiege.cours@gmail.com>
2023-03-01 16:13:28 +0100

    Ajout de la possibilité de suppression des tâches

--- script.js
+++ script.js
@@ -2,45 +2,56 @@
 new Vue({
     el: '#app',
     data: {
-      tasks: [],
-      newTask: {
-        description: '',
-        startDate: '',
-        endDate: '',
-        status: 'à faire',
-        priority: 'basse',
-      },
+        tasks: [],
+        newTask: {
+            description: '',
+            startDate: '',
+            endDate: '',
+            status: 'à faire',
+            priority: 'basse',
+        },
     },
-    
+    created() {
+        const savedtask = localStorage.getItem('tasks');
+        if (savedtask) {
+            this.tasks = JSON.parse(savedtask);
+        }
+    }
+    ,
+
     methods: {
-      addTask: function() {
-        const newTask = {
-          description: this.newTask.description,
-          startDate: this.newTask.startDate,
-          endDate: this.newTask.endDate,
-          status: this.newTask.status,
-          priority: this.newTask.priority,
-        };
-        this.tasks.push(newTask);
+        addTask: function () {
+            const newTask = {
+                description: this.newTask.description,
+                startDate: this.newTask.startDate,
+                endDate: this.newTask.endDate,
+                status: this.newTask.status,
+                priority: this.newTask.priority,
+            };
+            this.tasks.push(newTask);
 
-        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
 
-        this.newTask.description = '';
-        this.newTask.startDate = '';
-        this.newTask.endDate = '';
-        this.newTask.status = 'à faire';
-        this.newTask.priority = 'basse';      
+            this.newTask.description = '';
+            this.newTask.startDate = '';
+            this.newTask.endDate = '';
+            this.newTask.status = 'à faire';
+            this.newTask.priority = 'basse';
 
-        const message = document.getElementById('message');
-        message.style.display = 'block';
-        setTimeout(function() {
-          message.style.display = 'none';
-        }, 3500);
-      },
-      removeTask: function(index) {
-        // Suppression de la tâche correspondante
-        this.tasks.splice(index, 1);
-      },
+            const message = document.getElementById('message');
+            message.style.display = 'block';
+            setTimeout(function () {
+                message.style.display = 'none';
+            }, 3500);
+        },
+        removeTask: function (index) {
+            // Suppression de la tâche correspondante
+            this.tasks.splice(index, 1);
+
+
+
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+        },
     },
 });
   
